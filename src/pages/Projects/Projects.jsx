566ec70718c5da20548e287708402c9c3a7b2f1f
RuthShryock <ruthieshryock@gmail.com>
2023-01-27 17:04:44 -0500

    restructing modal view and adding inital project logos

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,9 +1,34 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import React from 'react';
+import React, {useState} from 'react';
 import './Projects.css';
 
 const Projects = () => {
+    const projectLogos = [{
+        photo: "/growth.png",
+        text:"ex"
+    },
+    {
+        photo: "/pantry.png",
+        text:"ex"
+    },
+    {
+        photo: "/dockerhound.png",
+        text: "ex"
+    },
+    {
+        photo: "/kg.png",
+        text: "ex"
+    },
+    {
+        photo: "/paradisepages.png",
+        text: "ex"
+    }
+    ]
+    // const [openModal, setOpenModal] = useState();
+    // const [imgsrc, setImagesrc] = useState();
+    // const [imgdescp, setImgdescp] = useState();
+
     return (
         <div className="projects">
              <div className="top-bar">
@@ -26,7 +51,36 @@ const Projects = () => {
                     </div>
                 </div>
             </div>
-        </div>   
+            <div className="bodyProject">
+                <div className="gridProjects">
+                    {projectLogos.map((e,i) => 
+                        <div className="outerPChild">
+                            <div className="gridProject-child" style={{ 
+                                backgroundImage: `url(${e.photo})` 
+                            }}></div>
+                            
+                        </div>
+                    )}
+                    {/* 
+                     onClick={() => {setOpenModal(true); setImagesrc(e.photo); setImgdescp(e.text)}}
+                    <Modal
+                        open={openModal === true}
+                        onClose={() => setOpenModal(false)}
+                        disableAutoFocus={true}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box className='modalBox'>
+                        <button className='xmark' onClick={() => setOpenModal(false)}>
+                            <FontAwesomeIcon icon={faXmark}/>
+                        </button>
+                        <img id="modalImage" src={imgsrc} alt={imgdescp}></img>
+                        </Box>
+                    </Modal> */}
+
+                </div>
+        </div>  
+    </div>  
     )
 };
 
