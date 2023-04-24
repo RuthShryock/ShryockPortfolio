e47ad67a707be39d56207ad9eb2b0951fc8f0ea2
RuthShryock <ruthieshryock@gmail.com>
2023-04-24 19:35:31 -0400

    styling and formatting about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -18,16 +18,16 @@ const About = () => {
                 <img className="flowerOne" src="flowerTwo.png" alt="decor"/>
                 <img className="flowerTwo" src="flowers.png" alt="decor"/>
             </div>
-            <img className="me" src="me.png" alt="profile"/>
-            <div className="nameInfo">
-                <h3>Ruth Shryock</h3>
-                <a href='https://www.linkedin.com/in/ruth-shryock/' target="_blank" rel="noopener noreferrer" className='icon'><FontAwesomeIcon icon={faLinkedin}/></a>
-                <a href='https://github.com/RuthShryock'target="_blank" rel="noopener noreferrer" className='icon'><FontAwesomeIcon icon={faGithubSquare}/></a>
-                <a href='https://www.instagram.com/ruthie_shryock/' target="_blank" rel="noopener noreferrer" className='icon'><FontAwesomeIcon icon={faInstagramSquare}/></a>
-                <br/><br/>
-                <a href={ShryockResume} target="_blank" rel="noreferrer"><button className='resume'>resume</button></a>
-            </div>
-            
+            <div className="vertical-box">
+                <div className="profile-img"><img className="profile" src="me.png" alt="profile"/></div>
+                <div className="name-text"><h3>Ruth Shryock</h3></div>
+                <div className="clickable-icons">
+                    <a href='https://www.linkedin.com/in/ruth-shryock/' target="_blank" rel="noopener noreferrer" className='icon'><FontAwesomeIcon icon={faLinkedin}/></a>
+                    <a href='https://github.com/RuthShryock'target="_blank" rel="noopener noreferrer" className='icon'><FontAwesomeIcon icon={faGithubSquare}/></a>
+                    <a href='https://www.instagram.com/ruthie_shryock/' target="_blank" rel="noopener noreferrer" className='icon'><FontAwesomeIcon icon={faInstagramSquare}/></a>
+                </div>
+                <div className="resume-btn"><a href={ShryockResume} target="_blank" rel="noreferrer"><button className="resume">resume</button></a></div>
+            </div> 
         </div>   
     )
 };
